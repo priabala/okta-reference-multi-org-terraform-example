f9a8c4a91de7561860e0d13e8954749a4e6f2fa7
priabala <68129521+priabala@users.noreply.github.com>
2023-11-13 20:05:19 -0800

    Update grouprules.tf
    Create griup rule - test_rule

--- grouprules.tf
+++ grouprules.tf
@@ -5,3 +5,11 @@ resource "okta_group_rule" "dmzadmin_grouprule" {
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = "user.department==\"DMZ Ops\""
 }
+
+resource "okta_group_rule" "test_rule" {
+  name              = "Test Rule"
+  status            = "ACTIVE"
+  group_assignments = ["${okta_group.test_group.id}"]
+  expression_type   = "urn:okta:expression:1.0"
+  expression_value  = "String.startsWith(user.firstName,\"somename\")"
+}
