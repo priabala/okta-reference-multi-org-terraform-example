cd6ba7560115242ef16a5f10e7e599871be7b0e6
priabala <68129521+priabala@users.noreply.github.com>
2023-11-14 19:13:48 -0800

    Update customer-portal.tf
    Add new OIDC app - example

--- modules/applications/customer-portal.tf
+++ modules/applications/customer-portal.tf
@@ -14,3 +14,19 @@ resource "okta_app_oauth" "customer_portal" {
   skip_users = true
   skip_groups = true
 }
+
+
+resource "okta_app_oauth" "example" {
+  status         = "ACTIVE"
+  label          = "Example Portal"
+  type           = "web"
+  redirect_uris  = ["https://localhost:44378/signin-oidc", "https://localhost:44378/Customer", "https://oauth.pstmn.io/v1/callback"]
+  post_logout_redirect_uris = ["https://localhost:44378/signout-callback-oidc"]
+  login_uri      = "https://localhost:4437811/signin-oidc"
+  grant_types    = ["client_credentials", "authorization_code", "refresh_token"]
+  response_types = ["code"]
+  consent_method = "TRUSTED"
+  refresh_token_rotation = "ROTATE"
+  skip_users = true
+  skip_groups = true
+}
