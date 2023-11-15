3bfe4817eeaa0165d3f300f090070267bbf53397
priabala <68129521+priabala@users.noreply.github.com>
2023-11-14 19:39:18 -0800

    Update app_signon_policies.tf

--- modules/applications/app_signon_policies.tf
+++ modules/applications/app_signon_policies.tf
@@ -10,3 +10,20 @@ resource "okta_app_signon_policy_rule" "magic_link" {
   type                        = "ASSURANCE"
   re_authentication_frequency = "PT2H"
 }
+
+
+resource "okta_app_oauth" "my_app" {
+  label                     = "My App"
+  type                      = "web"
+  grant_types               = ["authorization_code"]
+  redirect_uris             = ["http://localhost:3000"]
+  post_logout_redirect_uris = ["http://localhost:3000"]
+  response_types            = ["code"]
+  // this is needed to associate the application with the policy
+  authentication_policy     = okta_app_signon_policy.my_app_policy.id
+}
+
+resource "okta_app_signon_policy" "my_app_policy" {
+  name        = "My App Sign-On Policy"
+  description = "Authentication Policy to be used on my app."
+}
